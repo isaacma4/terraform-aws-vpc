33f93b2a6d43e7d7954015319b0f058383038467
Isaac Ma <isaac_ma@live.com>
2024-11-08 13:02:54 -0500

    Remove duplication of instance_tenancy parameter

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 # Internet VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
-  instance_tenancy     = "default"
+  instance_tenancy     = "dedicated"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
-  instance_tenancy     = "dedicated"
 
   tags = {
     Name = var.vpc_name
